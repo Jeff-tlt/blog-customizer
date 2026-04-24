5382c5ff9eb674e17321b906a0955968fc306107
Jeff-tlt <s_zhavoronkov@bk.ru>
2026-04-24 11:20:10 +0400

    fix: remove custom chrome path for playwright

--- playwright.config.js
+++ playwright.config.js
@@ -1,11 +1,7 @@
-// playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
 	use: {
 		browserName: 'chromium',
-		launchOptions: {
-			executablePath: '/usr/bin/google-chrome',
-		},
 	},
 });
